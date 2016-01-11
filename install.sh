e0cd9fa835fc664533fd9e1e46bd2157b9247101
Tuan Bach Van <tuan@legiti.co>
2016-01-11 09:12:14 +0700

    Update install script.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-
 sudo apt-get install python-appindicator
 sudo apt-get install python-notify
 
-sudo ln -s "$(pwd)/bing_wallpaper.py" /usr/local/bin/bing_wallpaper
-sudo chmod +x /usr/local/bin/bing_wallpaper
+sudo ln -s "$(pwd)/bing_wallpaper.py" /usr/bin/bing_wallpaper
+
+sudo chmod +x /usr/bin/bing_wallpaper
